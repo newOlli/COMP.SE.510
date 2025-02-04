c0a4ff2814f3dda36fc6318f29a96c08dad44b8e
Olli Lehmuskenttä <olli.lehmuskentta@tuni.fi>
2025-02-04 10:56:44 +0200

    ex1 setup

---
+++ vue-for-react-developers-main/vue-for-react-developers-exercise-1/vue-for-react-developers-exercise-1/src/components/Chat/ChatMessage/ChatMessage.vue
@@ -0,0 +1,43 @@
+<script setup>
+// A message in the chat.
+import { inject } from "vue";
+import ChatBubble from "./ChatBubble.vue";
+import Timestamp from "../../Timestamp.vue";
+
+const props = defineProps({
+  message: {
+    type: Object,
+    default: () => ({
+      type: null,
+      userId: null,
+      direction: null,
+      timestamp: null,
+    }),
+  },
+});
+
+// Inject the provided userId value into this component
+const userId = inject("userId");
+
+// Which direction is the bubble originating from?
+const direction = props.message.senderId === userId ? "right" : "left";
+</script>
+
+<template>
+  <ChatBubble v-if="props.message.type === 'text'" :direction="direction">
+    {{ message.content }}
+    <Timestamp :date="props.message.timestamp" />
+  </ChatBubble>
+
+  <!-- If the message type is not 'text', render something else. Perhaps an emoji?
+       Hint: See the README's resources section for a link to Vue's directives
+             There's something for conditional rendering there!
+  -->
+</template>
+
+<style scoped>
+.timestamp {
+  display: block;
+  text-align: right;
+}
+</style>
---
+++ vue-for-react-developers-main/vue-for-react-developers-exercise-1/vue-for-react-developers-exercise-1/src/components/Chat/Compose.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from "vue";
+import Input from "../generic/form/Input.vue";
+import Button from "../generic/form/Button.vue";
+
+// Store the text input value. This is a reactive 'state' variable.
+const text = ref("");
+
+/**
+ * Handle changes to the text input value
+ * @param {String} newValue New text value
+ */
+const onChange = (newValue) => {
+  text.value = newValue;
+};
+
+// Components emit events. Parent components listen to these events. Just like the DOM!
+const emit = defineEmits(["send"]);
+
+/**
+ * Sends a text message.
+ */
+function send() {
+  // Hint: Call 'emit' with the correct arguments.
+  //       Finally, clear the text input.
+}
+</script>
+
+<template>
+  <div class="compose">
+    <div class="message-row">
+      <!-- Hint: See if you could send the message by pressing the enter key in the text field.
+                 https://vuejs.org/guide/essentials/event-handling.html#key-modifiers
+      -->
+      <Input :value="text" placeholder="Type a message" @change="onChange" />
+
+      <!-- Hint: Call the 'send()' function when the button emits a 'click' event. -->
+      <Button icon="send" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.message-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+}
+/* This targets a nested component with scoped styles*/
+.compose:deep(.emoji-selector) {
+  margin-bottom: 0.5rem;
+}
+</style>
